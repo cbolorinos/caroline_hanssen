c132a4b2a5e62761d8ebcf1428553b74afe51cb5
Christian Bolorinos <CBolorinos@saturno.local>
2018-05-24 10:49:15 -0700

    some email changes

--- mail.php
+++ mail.php
@@ -4,9 +4,10 @@ $email = $_POST['email'];
 $call = $_POST['call'];
 $message = $_POST['message'];
 $subject = $_POST['subject'];
-$formcontent=" From: $name \n Add to Mailing List: $call \n Message: $message";
+$formcontent=" From: $name \n Email: $email \n Add to Mailing List: $call \n Message: $message";
 $recipient = "cbolorinos@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Location: thankyou.html";
+header('Location: thank_you.html');
+exit();
 ?>
