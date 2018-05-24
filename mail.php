b2d7394477006323c319719f0ade9db0ba228aad
Christian Bolorinos <CBolorinos@saturno.local>
2018-05-24 13:09:35 -0700

    email works unless theres an email in it

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $call = $_POST['call'];
 $message = $_POST['message'];
 $subject = $_POST['subject'];
 $formcontent=" From: $name \n Email: $email \n Add to Mailing List: $call \n Message: $message";
-$recipient = "cbolorinos@gmail.com";
+$recipient = "caroline.hanssen@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 header('Location: thank_you.html');
