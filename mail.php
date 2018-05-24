adc348fba46462fc07f0b970dccb8d1fd2cdcd45
Christian Bolorinos <christianbolorinos@Christians-Mac-mini.local>
2018-05-23 19:31:44 -0700

    contact form

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$call = $_POST['call'];
+$message = $_POST['message'];
+$subject = $_POST['subject'];
+$formcontent=" From: $name \n Add to Mailing List: $call \n Message: $message";
+$recipient = "cbolorinos@gmail.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
