257feea2912f03b7bb84579c843b1024c6d870ec
Christian Bolorinos <CBolorinos@saturno.local>
2018-05-29 09:25:57 -0700

    contact form should fuckin work damnit

--- mail.php
+++ mail.php
@@ -6,7 +6,6 @@ $message = $_POST['message'];
 $subject = $_POST['subject'];
 $formcontent=" From: $name \n Email: $email \n Add to Mailing List: $call \n Message: $message";
 $recipient = "caroline.hanssen@gmail.com";
-$mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 header('Location: thank_you.html');
 exit();
