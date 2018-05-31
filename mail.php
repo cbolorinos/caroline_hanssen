67b41ded744a43505ac9f50ef23ac76f399fb1e5
Christian Bolorinos <CBolorinos@saturno.local>
2018-05-31 08:46:25 -0700

    contact fooorm dought

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $call = $_POST['call'];
 $message = $_POST['message'];
 $subject = $_POST['subject'];
 $formcontent=" From: $name \n Email: $email \n Add to Mailing List: $call \n Message: $message";
-$recipient = "caroline.hanssen@gmail.com";
+$recipient = "miminothing1@hotmail.com";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 header('Location: thank_you.html');
 exit();
